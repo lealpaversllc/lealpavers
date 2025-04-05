50b24ccbc153d4551197e32d4ba769ffe955e965
Messias <matheus.jf@live.comma200596theus>
2025-04-05 10:39:05 -0300

    fix: flat icon .ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,19 +17,15 @@ const poppinsSans = Poppins({
 
 export const metadata: Metadata = {
   title: 'Leal Pavers',
-  description:
-    'Transform your space with professional services from Leal Pavers.',
   openGraph: {
     title: 'Leal Pavers',
-    description:
-      'Transform your space with professional services from Leal Pavers.',
     url: 'https://lealpaversllc.com',
     siteName: 'Leal Pavers',
     images: [
       {
         url: 'https://lealpaversllc.com/assets/logo.svg',
-        width: 1200,
-        height: 630,
+        width: 215,
+        height: 105,
         alt: 'Image of Leal Pavers services',
       },
     ],
